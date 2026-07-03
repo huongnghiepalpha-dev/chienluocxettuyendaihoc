50fe21b979fcfae74363d5e2b36c8ce760c15d0f
huongnghiepalpha-dev <huongnghiepalpha@gmail.com>
2026-07-03 12:50:41 +0700

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+function App() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Chào mừng đến với Chiến lược Xét tuyển 2026</h1>
+    </div>
+  )
+}
+export default App
